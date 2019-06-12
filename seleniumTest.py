54fda8ab74612444b36a2583916e62cd7eec5dd6
marko <chekfu20@gmail.com>
2019-06-12 13:44:12 +0200

    click & close "news"

--- seleniumTest.py
+++ seleniumTest.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 driver = webdriver.Chrome()
 driver.get("http://www.python.org")
+driver.maximize_window()
 assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
+
+element = driver.find_element_by_id("news")
+element.click()
 driver.close()
