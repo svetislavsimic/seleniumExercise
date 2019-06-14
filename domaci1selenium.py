ff4e75a630c7adafa11649733189d41028ecd7f4
Nibzmeister <petarpeca99@gmail.com>
2019-06-14 14:10:08 +0200

    promenjen assert i dodat driver.close

--- domaci1selenium.py
+++ domaci1selenium.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 #from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elm = webdriver.Chrome.find_element_by_xpath(driver, "//body[@id='homepage']/div[@id='touchnav-wrapper']/header[@class='main-header']/div[@class='container']/nav[@id='mainnav']/ul[@class='navigation menu']/li[@id='news']/a[1]")
 elm.click()
-assert "No results found." not in driver.page_source
+assert "Our Blogs | Python.org" in driver.title
+time.sleep(5)
+driver.close()
