0552511d24c02c083829b9957e748ae1081faec2
nevenamioch94 <nevena.miocinovic@gmail.com>
2019-06-18 00:49:55 +0200

    Uradjen domaci1106

---
+++ domaci1106.py
@@ -0,0 +1,36 @@
+#Za domaci 11.06. :
+#* otvorite stranicu https://www.python.org/
+#* kliknite na tab News
+#***************************************************************
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+import time
+
+class TestClickNews(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+
+
+    def Click_on_News(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        s1 = Select(driver.find_element_by_name("News from around the Python world"))
+
+        s1.select_by_visible_text('Python News')
+
+        for opt in s1.options:
+            print(opt.text)
+            s1.select_by_visible_text(opt.text)
+            time.sleep(5)
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+
