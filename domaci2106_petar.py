89cf8b85db026cd1b05c361f507d399c1d1e80a2
unknown <petarpeca99@gmail.com>
2019-06-25 19:35:07 +0200

    idk

--- domaci2106_petar.py
+++ domaci2106_petar.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.common.action_chains import ActionChains
 
 class TestPythonOrg(unittest.TestCase):
     def setUp(self):
@@ -15,14 +16,16 @@ class TestPythonOrg(unittest.TestCase):
         svetelm = driver.find_element_by_xpath("(//a[contains(.,'Svet')])[1]")
         svethref = svetelm.get_attribute('href')
         driver.execute_script("window.open('{}')".format(svethref))
-        time.sleep(3)
+        driver.implicitly_wait(10)
         clanakelm = driver.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/section[1]/div[2]/section[1]/article[1]/div[1]/h3[1]/a[1]")
         print(clanakelm.get_attribute("href"))
 
     def testNews(self):
         driver = self.driver
         driver.get("https://www.blic.rs/")
-        a = driver.find_element_by_xpath("//div[@class='owl-item active']//a[@class='custom-shareFb'][contains(text(),'Share')]")
+        a = driver.find_element_by_xpath("(//a[@href='#'][contains(.,'Share')])[7]")
+        action = ActionChains(driver)
+        action.move_to_element(a).perform()
         a.click()
     def tearDown(self):
         self.driver.close()
