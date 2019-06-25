2c204e0688a2e9bab1ab732d6ae965724d1cd184
marko <chekfu20@gmail.com>
2019-06-25 19:06:28 +0200

    dodana petlja Opcija 1.

--- testToolsQa.py
+++ testToolsQa.py
@@ -1,32 +1,52 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
-import time
+from time import sleep
+import datetime
+
 
 class ToolsQa(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.driver.maximize_window()
 
-    def simaNestoKuca(self):
-        print("Zdravo svete!")
-
-    def test_search_in_python_org(self):
+    def test_form_tools_qa(self):
+        a = datetime.datetime.now()
         driver = self.driver
         driver.get('http://www.toolsqa.com/automation-practice-form/')
-        s1 = Select(driver.find_element_by_id('continents'))
+        #s1 = Select(driver.find_element_by_xpath('continents'))
+        element = driver.find_element_by_xpath("//input[@name='firstname']")
+        element.clear()
+        element.send_keys('Marko')
+        element = driver.find_element_by_xpath("//input[@name='lastname']")
+        element.clear()
+        element.send_keys('Fucek')
+        element = driver.find_element_by_xpath("//input[contains(@value,'Male')]").click()
+        element = driver.find_element_by_xpath("//input[contains(@value,'1')]").click()
+        element = driver.find_element_by_xpath("//input[contains(@id,'datepicker')]").send_keys(a.strftime("%d.%m.%Y"))
+        element = driver.find_element_by_xpath("//input[@value='Automation Tester']").click()
+        element = driver.find_element_by_xpath("//input[contains(@id,'tool-2')]").click()
+        s1 = Select(driver.find_element_by_xpath("//select[contains(@id,'continents')]"))
+
+
+
+
+
+        #element.Select(driver.find_element_by_xpath("//input[contains(@value,'Male')]")).click()
+        #sleep(4)
+
 
         s1.select_by_visible_text('Europe')
 
-        for opt in s1.options:
-            print(opt.text)
-            s1.select_by_visible_text(opt.text)
-            time.sleep(10)
-
+        # for opt in s1.options:
+        #     print(opt.text)
+        #     s1.select_by_visible_text(opt.text)
+        #     time.sleep(10)
 
     def tearDown(self):
         self.driver.close()
+
 
 if __name__ == "__main__":
     unittest.main()
