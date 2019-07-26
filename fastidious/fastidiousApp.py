5d56890da697eb08169709448d7d91c7a2dc8c46
marko <chekfu20@gmail.com>
2019-07-26 12:31:12 +0200

    dodan test InOut

--- fastidious/fastidiousApp.py
+++ fastidious/fastidiousApp.py
@@ -5,7 +5,8 @@ from openpyxl import load_workbook
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 
 class Fastidious(unittest.TestCase):
 
@@ -188,6 +189,68 @@ class Fastidious(unittest.TestCase):
         )))
         assert 'Unable to login. Try again!' in driver.page_source
         sheet.cell(row=8, column=4).value = "PASS"
+        book.save(self.filePath)
+
+    def testInOutInOut(self):
+        driver = self.driver
+        book = load_workbook(self.filePath)
+        sheet = book["login"]
+        driver.get("https://fastidious-app.netlify.com")
+        driver.maximize_window()
+        action = ActionChains(driver)
+        assert "Fastidious Admin" in driver.title
+        wait = WebDriverWait(driver, 20)
+        logged_in_url = r"https://fastidious-app.netlify.com/dashboard"
+
+        user_field = driver.find_element_by_xpath("//input[contains(@name,'email')]")
+        password_field = driver.find_element_by_xpath("//input[contains(@name,'password')]")
+        login_btn = driver.find_element_by_xpath("//button[@type='submit']")
+
+        user_field.send_keys(sheet['B2'].value)  # user ok
+        password_field.send_keys(sheet['C7'].value)  # pass nok
+        login_btn.click()
+
+        wait.until(EC.visibility_of_element_located((
+            By.XPATH, "//div[@class='message'][contains(.,'Unable to login. Try again!')]"
+        )))
+        assert 'Unable to login. Try again!' in driver.page_source
+
+        password_field.send_keys(Keys.CONTROL + "a")
+        password_field.send_keys(Keys.DELETE)
+        password_field.send_keys(sheet['C6'].value)  # pass ok
+        action.send_keys(Keys.ENTER).perform()
+
+        wait.until(EC.url_to_be(logged_in_url))
+        assert driver.current_url == logged_in_url
+        driver.find_element_by_xpath("//i[@class='fas fa-sign-out-alt']").click()  # go back to login
+
+        wait.until(EC.url_to_be(r"https://fastidious-app.netlify.com/login"))
+        assert r"https://fastidious-app.netlify.com/login" in driver.current_url
+
+        # log in again
+        driver.find_element_by_name("email").send_keys(sheet['B6'].value)  # user nok
+        driver.find_element_by_name("password").send_keys(sheet['C2'].value)  # pass ok
+        action.send_keys(Keys.ENTER).perform()
+
+        wait.until(EC.visibility_of_element_located((
+            By.XPATH, "//div[@class='message'][contains(.,'Unable to login. Try again!')]"
+        )))
+        assert 'Unable to login. Try again!' in driver.page_source
+
+        username = driver.find_element_by_name("email")
+        username.send_keys(Keys.CONTROL + "a")
+        username.send_keys(Keys.DELETE)
+        username.send_keys(sheet['B2'].value) # user ok
+        action.send_keys(Keys.ENTER).perform()
+
+        wait.until(EC.url_to_be(logged_in_url)) # logged in
+        assert driver.current_url == logged_in_url
+        driver.find_element_by_xpath("//i[@class='fas fa-sign-out-alt']").click()
+
+        wait.until(EC.url_to_be(r"https://fastidious-app.netlify.com/login"))
+        assert r"https://fastidious-app.netlify.com/login" in driver.current_url
+
+        sheet.cell(row=9, column=4).value = "PASS"
         book.save(self.filePath)
 
     def tearDown(self):
